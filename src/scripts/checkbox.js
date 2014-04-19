992585ea06156c7db7248528534e045ac524fad4
Gustavo Henke <gustavo@injoin.com.br>
2014-04-19 14:01:13 -0300

    feat(radio directive): created radio directive

--- src/scripts/checkbox.js
+++ src/scripts/checkbox.js
@@ -1,48 +1,53 @@
 !function( ng ) {
     "use strict";
 
-    var template = "<span class='checkbox' tabindex='0'></span>";
+    var baseTemplate = "<span class='DIRECTIVE' tabindex='0'></span>";
 
     var module = ng.module( "frontkit.checkbox", [
         "frontkit.utils"
     ]);
 
-    module.directive( "checkbox", [
-        "$compile",
-        "$timeout",
-        "keycodes",
-        function( $compile, $timeout, keycodes ) {
-            var definition = {};
+    [ "checkbox", "radio" ].forEach(function( directive ) {
+        var template = baseTemplate.replace( "DIRECTIVE", directive );
 
-            definition.restrict = "A";
-            definition.link = function( scope, input, attr ) {
-                var styled = $compile( template )( scope );
-                input.after( styled );
-                input.addClass( "hide" );
+        module.directive( directive, [
+            "$compile",
+            "$timeout",
+            "keycodes",
+            function( $compile, $timeout, keycodes ) {
+                var definition = {};
 
-                styled.on( "keypress", function( evt ) {
-                    // Space is the only key that triggers an click on a checkbox, the far we know
-                    if ( evt.type === "keypress" && evt.which !== keycodes.SPACE ) {
-                        return;
-                    }
+                definition.restrict = "A";
+                definition.link = function( scope, input, attr ) {
+                    var styled = $compile( template )( scope );
+                    input.after( styled );
+                    input.addClass( "hide" );
 
-                    styled.triggerHandler( "click" );
-                    evt.preventDefault();
-                });
+                    styled.on( "keypress", function( evt ) {
+                        // Space is the only key that triggers an click on a checkbox/radio, the far
+                        // we know
+                        if ( evt.type === "keypress" && evt.which !== keycodes.SPACE ) {
+                            return;
+                        }
 
-                styled.on( "click", function( evt ) {
-                    evt.preventDefault();
-
-                    input.prop( "checked", !input.prop( "checked" ) );
-                    $timeout(function() {
-                        // Activate Angular's native checkbox directive
-                        input.triggerHandler( "click" );
+                        styled.triggerHandler( "click" );
+                        evt.preventDefault();
                     });
-                });
-            };
 
-            return definition;
-        }
-    ]);
+                    styled.on( "click", function( evt ) {
+                        evt.preventDefault();
+
+                        input.prop( "checked", !input.prop( "checked" ) );
+                        $timeout(function() {
+                            // Activate Angular's native checkbox directive
+                            input.triggerHandler( "click" );
+                        });
+                    });
+                };
+
+                return definition;
+            }
+        ]);
+    });
 
 }( angular );
---
+++ test/unit/specs/radio.spec.js
@@ -0,0 +1,83 @@
+describe( "Radio Directive", function() {
+    "use strict";
+
+    var $rootScope, $compile, $document, $timeout, keycodes;
+    var expect = chai.expect;
+
+    beforeEach( module( "frontkit.checkbox" ) );
+    beforeEach( inject(function( $injector ) {
+        // Inject tons of things
+        $rootScope = $injector.get( "$rootScope" );
+        $compile = $injector.get( "$compile" );
+        $document = $injector.get( "$document" );
+        $timeout = $injector.get( "$timeout" );
+        keycodes = $injector.get( "keycodes" );
+
+        // Compile a few other things
+        this.input = $( "<input type='radio' radio />" );
+        this.input.appendTo( "body" );
+        $compile( this.input )( $rootScope );
+        $rootScope.$apply();
+
+        this.styled = this.input.next();
+    }));
+
+    afterEach(function() {
+        this.input.remove();
+    });
+
+    it( "should exist", inject(function( $injector ) {
+        var getDirective = function() {
+            return $injector.get( "radioDirective" );
+        };
+
+        expect( getDirective ).to.not.throw( Error );
+    }));
+
+    it( "should add .radio right after the element", function() {
+        var cls = this.styled.attr( "class" ).split( " " );
+        expect( cls ).to.contain( "radio" );
+    });
+
+    it( "should have a tabindex defined", function() {
+        var tabindex = +this.styled.attr( "tabindex" );
+        tabindex = isNaN( tabindex ) ? -1 : tabindex;
+
+        expect( tabindex ).to.be.gte( 0 );
+    });
+
+    describe( "on keypress space", function() {
+        it( "should trigger the click event", function() {
+            var spy = sinon.spy();
+            var event = $.Event( "keypress", {
+                which: keycodes.SPACE
+            });
+
+            this.styled.on( "click", spy );
+            this.styled.trigger( event );
+
+            expect( spy.called ).to.be.ok;
+        });
+    });
+
+    describe( "on click", function() {
+        it( "should invert checked state", function () {
+            this.styled.triggerHandler( "click" );
+            expect( this.input.prop( "checked" ) ).to.be.ok;
+
+            this.styled.triggerHandler( "click" );
+            expect( this.input.prop( "checked" ) ).to.not.be.ok;
+        });
+
+        it( "should trigger input click", function() {
+            var spy = sinon.spy();
+            this.input.on( "click", spy );
+            this.styled.triggerHandler( "click" );
+
+            // TODO findout why $rootScope.$apply() no longer works for timeouts
+            $timeout.flush();
+            expect( spy.called ).to.be.ok;
+        });
+    });
+
+});
