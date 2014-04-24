6abf7af7e1047ca516f0bb76a5c7ce8cc0098582
Gustavo Henke <gustavo@injoin.com.br>
2014-04-24 08:44:33 -0300

    feat(docs menu): add a submenu to currently active item
    To enhance the docs navigation, a submenu was added (via directive) to the currently active item. It's populated from the page markup, based on the docsSectionTitle swig macro, on a 1-level deep basis.

--- assets/scripts/docs.js
+++ assets/scripts/docs.js
@@ -1,5 +1,41 @@
 !function( ng ) {
     "use strict";
 
-    ng.module( "frontkit-docs", [ "frontkit" ] );
+    var module = ng.module( "frontkit-docs", [ "frontkit" ] );
+
+    module.directive( "submenu", [
+        "$document",
+        function( $document ) {
+            var document = $document[ 0 ];
+            var definition = {};
+
+            definition.scope = {};
+            definition.template =
+                "<ul class='nav nav-list'>" +
+                    "<li class='nav-item' ng-repeat='heading in headings'>" +
+                        "<a href='{{ heading.href }}'>{{ heading.text }}</a>" +
+                    "</li>" +
+                "</ul>";
+
+            definition.link = function( scope ) {
+                var headings = document.querySelectorAll(
+                    ".docs-content > section > .docs-heading"
+                );
+                scope.headings = [];
+
+                [].forEach.call( headings, function( heading ) {
+                    var text = heading.innerText;
+                    var anchor = heading.querySelector( ".docs-anchor" );
+                    text = text.replace( anchor.innerText, "" );
+
+                    scope.headings.push({
+                        href: anchor.href,
+                        text: text.trim()
+                    });
+                });
+            };
+
+            return definition;
+        }
+    ]);
 }( angular );
