dba254542deadeb66289ba7cd9647740465b16f5
Gustavo Henke <gustavo@injoin.com.br>
2014-06-15 02:37:14 -0300

    chore(coverage): change coverage location and ignore it

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -43,7 +43,7 @@ module.exports = function( config ) {
 
         coverageReporter: {
             type: "lcov",
-            dir: "coverage/"
+            dir: "test/coverage/"
         },
 
         // Stop running after 1.5 minutes
