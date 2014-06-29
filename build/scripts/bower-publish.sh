3aa5fe3fa5f232efd365d711d3f7790eb4bc47e2
Gustavo Henke <gustavo@injoin.com.br>
2014-06-29 17:33:23 -0300

    chore(scripts, .travis.yml): fix post-tag automation scripts
    Use /bin/bash, for example

--- build/scripts/bower-publish.sh
+++ build/scripts/bower-publish.sh
@@ -1,29 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! -z "${TRAVIS_TAG}" ]; then
     # Setup deploy key
-    rm -rf ~/.ssh/
+    rm -rf ~/.ssh
+    mkdir ~/.ssh
     echo -n $bower_id_rsa_{00..30} >> ~/.ssh/id_rsa_base64
     base64 --decode --ignore-garbage ~/.ssh/id_rsa_base64 > ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa
     echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
     # Create vars with the latest commit author name and email
-    GIT_NAME = `git show --format="%an" master | head -n 1`
-    GIT_MAIL = `git show --format="%ae" master | head -n 1`
+    GIT_NAME=`git show --format="%an" | head -n 1`
+    GIT_MAIL=`git show --format="%ae" | head -n 1`
 
-    # Configure git and clone repo
-    git config user.name "${GIT_NAME}"
-    git config user.email "${GIT_MAIL}"
+    # Clone repo
     git clone git@github.com:injoin/bower-frontkit.git
 
-    # Unzip the package into the git repo, copy the bower.json file and cd into it
+    # Unzip the package into the git repo, copy the bower.json file
     unzip frontkit.dev.zip -d bower-frontkit
     cp bower.json bower-frontkit/bower.json
+
+    # cd into the repo and configure git
     cd bower-frontkit
+    git config user.name "${GIT_NAME}"
+    git config user.email "${GIT_MAIL}"
 
     # Commit, tag and push
-    git commit -am "chore(release): release v${TRAVIS_TAG}"
+    git add .
+    git commit -m "chore(release): release v${TRAVIS_TAG}"
     git tag "${TRAVIS_TAG}"
     git push origin master --tags
 
--- build/scripts/docs-update.sh
+++ build/scripts/docs-update.sh
@@ -1,15 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! -z "${TRAVIS_TAG}" ]; then
     # Setup deploy key
-    rm -rf ~/.ssh/
+    rm -rf ~/.ssh
+    mkdir ~/.ssh
     echo -n $self_id_rsa_{00..30} >> ~/.ssh/id_rsa_base64
     base64 --decode --ignore-garbage ~/.ssh/id_rsa_base64 > ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa
     echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
+    # Create vars with the latest commit author name and email
+    GIT_NAME=`git show --format="%an" | head -n 1`
+    GIT_MAIL=`git show --format="%ae" | head -n 1`
+
+    # Configure git
+    git config user.name "${GIT_NAME}"
+    git config user.email "${GIT_MAIL}"
+
+    # Drop modifications
+    git reset --hard
+    git clean -f
+
     # Checkout gh-pages and merge changes from master
-    git checkout gh-pages
+    git fetch origin gh-pages:gh-pages
+    git checkout -f gh-pages
     git merge master
 
     # Update references to a few compiled files
@@ -23,5 +37,6 @@ if [ ! -z "${TRAVIS_TAG}" ]; then
 
     # Commit and push stuff
     git commit -m "chore(release): docs release v${TRAVIS_TAG}"
+    git remote set-url origin git@github.com:injoin/frontkit.git
     git push origin gh-pages
 fi
