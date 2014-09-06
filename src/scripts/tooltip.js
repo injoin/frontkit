7404a3ea4d855ed0656f62d8d8549b0758a846eb
Gustavo Henke <gustavo@injoin.io>
2014-09-06 16:44:40 -0300

    style(tooltip): add a few comments

--- src/scripts/tooltip.js
+++ src/scripts/tooltip.js
@@ -14,13 +14,18 @@
                 var timeout;
                 var title = element.attr( "title" );
 
+                // We shouldn't use .removeAttr() because if tooltip is applied in a <abbr>,
+                // for example, the styles (which are required to demonstrate something) would
+                // be lost.
                 element.attr( "title", "" );
 
                 element.on( "mouseenter", function() {
+                    // Schedule showing the tooltip after 300 ms
                     timeout = setTimeout(function() {
                         var rect = element[ 0 ].getBoundingClientRect();
                         timeout = null;
 
+                        // Update the tooltip content, show it and position it
                         tooltip.text( title ).addClass( "visible" );
                         tooltip.css({
                             top: rect.bottom + "px",
@@ -30,12 +35,15 @@
                 });
 
                 element.on( "mouseleave", function() {
+                    // If the timeout for showing the tooltip has not been triggered yet,
+                    // let's ensure that this doesn't happen by clearing it
                     if ( timeout ) {
                         clearTimeout( timeout );
                         timeout = null;
                         return;
                     }
 
+                    // ...otherwise, just remove .visible from the element
                     tooltip.removeClass( "visible" );
                 });
             };
